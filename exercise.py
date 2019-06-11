6ff1bbaa412e26f2edb4478cb24fcba5983e0a81
Jessica Moore <jessica.cmoore1@gmail.com>
2019-06-11 15:18:22 -0400

    Reformatting

--- exercise.py
+++ exercise.py
@@ -16,12 +16,12 @@ project = {
     ],
 }
 
-project_steps = project['steps']
+project_steps = project["steps"]
 steps_assigned = 0
 
 while steps_assigned < len(project_steps):
-    for committee_member in project['committee']:
-        project_steps[steps_assigned]['committee_member'] = committee_member
+    for committee_member in project["committee"]:
+        project_steps[steps_assigned]["committee_member"] = committee_member
         steps_assigned += 1
 
 for step in project_steps:
@@ -36,8 +36,3 @@ for step in project_steps:
 # {'description': 'final edit', 'due_date': 'October 1, 2019', 'committee_member': 'Stella'}
 # {'description': 'final version', 'due_date': 'November 20, 2019', 'committee_member': 'Salma'}
 # {'description': 'Wrap up', 'due_date': 'December 1, 2019', 'committee_member': 'Kai'}
-
-
-
-
-
